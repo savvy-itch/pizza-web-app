eab3f33072b434684566a44c6eb81e50d925df74
Mik-Halo <sprone3@gmail.com>
2024-02-14 20:41:48 +0200

    improve optimization & SEO, responsiveness; add deployment packages;

--- app.js
+++ app.js
@@ -2,12 +2,20 @@ require('dotenv').config();
 require('express-async-errors');
 const express = require('express');
 const app = express();
-const cors = require('cors');
+// const cors = require('cors');
 const mongoose = require('mongoose');
 
 const pizzasRouter = require('./routes/pizzas');
 const ingredientRouter = require('./routes/ingredients');
 const emailRouter = require('./routes/sendEmail');
+
+const compression = require("compression");
+const helmet = require('helmet');
+const RateLimit = require('express-rate-limit');
+const limiter = RateLimit({
+  windowMs: 1* 60 * 1000,
+  max: 20
+})
 
 const errorMiddleware = require('./middleware/error-handler');
 const notFoundMiddleware = require('./middleware/not-found');
@@ -15,11 +23,15 @@ const connectDB = require('./db/connect');
 
 const port = process.env.PORT || 3000;
 
+app.use(compression());
+app.use(helmet());
+app.use(limiter);
+
 app.use(express.static('public'));
 app.use(express.json());
 
 // CHECK IF CORS SHOULD BE REMOVED BEFORE DEPLOYMENT
-app.use(cors());
+// app.use(cors());
 
 app.use('/api/pizzas', pizzasRouter);
 app.use('/api/ingredients', ingredientRouter);
