7b9d3da6b08de3cbdaf4a9a34ed03a7326f0e110
Mik-Halo <sprone3@gmail.com>
2024-02-15 21:40:34 +0200

    set production url

--- public/scripts/globals.js
+++ public/scripts/globals.js
@@ -1,3 +1,9 @@
-export const pizzasUrl =  'http://localhost:3000/api/pizzas';
-export const ingredientsUrl = 'http://localhost:3000/api/ingredients';
+export const pizzasUrl =  getApiUrl('api/pizzas');
+export const ingredientsUrl = getApiUrl('api/ingredients');
 export const numOfSkeletons = 4;
+
+function getApiUrl(endpoint) {
+  const isProduction = window.location.hostname.includes('https://prog-pizza-website.netlify.app/');
+  const baseUrl = isProduction ? '' : 'http://localhost:3000/';
+  return `${baseUrl}${endpoint}`;
+}
