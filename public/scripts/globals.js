de485db67dfc209d603ccd10afa005db4f696e4a
Mik-Halo <sprone3@gmail.com>
2024-02-19 20:30:43 +0200

    fix production settings

--- public/scripts/globals.js
+++ public/scripts/globals.js
@@ -1,9 +1,10 @@
-export const pizzasUrl =  getApiUrl('api/pizzas');
-export const ingredientsUrl = getApiUrl('api/ingredients');
+export const pizzasUrl =  'api/pizzas';
+export const ingredientsUrl = 'api/ingredients';
 export const numOfSkeletons = 4;
 
-function getApiUrl(endpoint) {
-  const isProduction = window.location.hostname.includes('prog-pizza-website.netlify.app');
-  const baseUrl = isProduction ? '' : 'http://localhost:3000/';
-  return `${baseUrl}${endpoint}`;
-}
+// function getApiUrl(endpoint) {
+//   const isProduction = window.location.hostname.includes('prog-pizza-website.netlify.app');
+//   const baseUrl = isProduction ? '' : 'http://localhost:3000/';
+//   console.log(`${baseUrl}${endpoint}`)
+//   return `${baseUrl}${endpoint}`;
+// }
