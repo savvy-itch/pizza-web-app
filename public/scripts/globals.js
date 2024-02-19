92bfac8edb8903cffc5cffd68a992e986e8292cb
Mik-Halo <sprone3@gmail.com>
2024-02-19 13:53:03 +0200

    improve browser compatibility of price range filter;

--- public/scripts/globals.js
+++ public/scripts/globals.js
@@ -3,7 +3,7 @@ export const ingredientsUrl = getApiUrl('api/ingredients');
 export const numOfSkeletons = 4;
 
 function getApiUrl(endpoint) {
-  const isProduction = window.location.hostname.includes('https://prog-pizza-website.netlify.app/');
+  const isProduction = window.location.hostname.includes('https://prog-pizza-website.netlify.app');
   const baseUrl = isProduction ? '' : 'http://localhost:3000/';
   return `${baseUrl}${endpoint}`;
 }
